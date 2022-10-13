b0bcc19a46d20a4f0438c872dc42f9e3d9bc80e3
Nevin <nevinbaiju@gmail.com>
2022-10-13 00:59:36 -0400

    defeat

---
+++ Top Interview Questions/Medium/06_dynamic_programming_03.py
@@ -0,0 +1,18 @@
+from re import A
+from typing import List
+
+class Solution:
+    def coinChange(self, coins: 'List[int]', amount: 'int') -> 'int':
+            dp = [0] + [float('inf') for i in range(amount)]
+            for i in range(1, amount+1):
+                for coin in coins:
+                    if i - coin >= 0:
+                        dp[i] = min(dp[i], dp[i-coin] + 1)
+            if dp[-1] == float('inf'):
+                return -1
+            return dp[-1]
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.coinChange([1,2,5], 50))
