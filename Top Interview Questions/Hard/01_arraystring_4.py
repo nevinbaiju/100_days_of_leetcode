096b44e2c5fcba03cb999bb323b810f859e4505b
Nevin <nevinbaiju@gmail.com>
2022-10-27 19:56:43 -0400

    Solved spiral matrix

---
+++ Top Interview Questions/Hard/01_arraystring_4.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        print(height)
+
+if __name__ == "__main__":
+    s = Solution()
+    s.maxArea([1,8,6,2,5,4,8,3,7])
